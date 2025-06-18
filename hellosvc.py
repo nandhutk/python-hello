6d155bf487ec694132686fba2d414aff2d3241fc
nandhutk <97139992+nandhutk@users.noreply.github.com>
2025-06-18 16:47:10 +0530

    Update hellosvc.py

--- hellosvc.py
+++ hellosvc.py
@@ -2,11 +2,10 @@ apiVersion: v1
 kind: Service
 metadata:
  labels:
- k8s-app: k8s_python_demo_code
- name: hellopy
+  app: hello-py
 spec:
  type: NodePort
  ports:
  — port: 4025
  selector:
- k8s-app: k8s_python_demo_code
+   app: hello-py
